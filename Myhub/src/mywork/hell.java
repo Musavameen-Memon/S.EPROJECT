7276a2b41b96db779c2595009422c8bc9392aecb
Lenovo <Lenovo@DESKTOP-VJ30NBA>
2023-05-15 11:57:39 +0500

    First Commit

---
+++ Myhub/src/mywork/hell.java
@@ -0,0 +1,12 @@
+package mywork;
+
+public class hell {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 world WorldObject = new world();
+	 WorldObject.complexMessage ();
+
+	}
+
+}
---
+++ Myhub/src/mywork/world.java
@@ -0,0 +1,13 @@
+package mywork;
+
+public class world {
+	   public void complexMessage() {
+		   System.out.println("learning hub");
+	   }
+	   
+
+		// TODO Auto-generated method stub
+
+	}
+
+
